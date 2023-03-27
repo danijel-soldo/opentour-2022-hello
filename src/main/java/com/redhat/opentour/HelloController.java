6b04b38bcd7f09bc8defd8475bfd522d8ff0d6b5
Danijel Soldo <64021215+danijel-soldo@users.noreply.github.com>
2023-03-27 21:24:45 +0200

    Update HelloController.java
    final arch

--- src/main/java/com/redhat/opentour/HelloController.java
+++ src/main/java/com/redhat/opentour/HelloController.java
@@ -9,7 +9,8 @@ public class HelloController {
     private static final String LOCATION = System.getenv().getOrDefault("LOCATION", "planet earth");
 
     public static void sayHello(Context ctx) {
-        ctx.result(format("Hello uu SHOW world ajmeeee from %s!", LOCATION));
+       // ctx.result(format("Hello uu SHOW world ajmeeee from %s!", LOCATION));
+        ctx.result(format("Hello Bosch Architecture Week!"));
     };
 
 }
